8a6462723acc31722ea27c015374c46beb6cedd9
cpuikin <46723287+cpuikin@users.noreply.github.com>
2020-08-24 11:05:48 +0800

    commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('HI')
+#create a function all cpital letter and 
+#punctuation in a string. Return the clean string.
+
+def clean_str1(str1):
+    new_str = str1 
+    for i in str1:
+        if i.isupper():
+            new_str = str1.replace(i, '')
+
+    return new_str
+
+print (clean_str1('Call Me Maybe'))            
